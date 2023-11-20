e427c5bd9833a82f8746dcb3b1124f6a72ed3f2c
Nikita Vasilev <nv3212@gmail.com>
2023-11-20 19:40:40 +0300

    Generate the initial project structure

---
+++ Sources/Lazy/Classes/Lazy.swift
@@ -0,0 +1,6 @@
+//
+// Lazy
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+final class Lazy {}
---
+++ Tests/LazyTests/LazyTests.swift
@@ -0,0 +1,8 @@
+//
+// Lazy
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+import XCTest
+
+final class LazyTests: XCTestCase {}
